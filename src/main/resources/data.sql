369317f2801c9515798baa5c6a74df7e5fe94c64
Erkan Ozsoy <erkan.ozsoy@bil.omu.edu.tr>
2019-03-02 17:06:49 +0300

    Update data.sql
    names changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,4 @@ insert into alien values (103,'Justin Timberlake','Lawyer');
 insert into alien values (104,'Dimitri Vegas','Student');
 insert into alien values (105,'Ummet Ozcan','Singer');
 insert into alien values (107,'Martin Garrix','Computer Engineer');
+insert into alien values (105,'Ummet Jackson','Singer');
