bf84c3474f3e002f512b299dbfa98edd113203e3
Erkan Ozsoy <erkan.ozsoy@bil.omu.edu.tr>
2019-03-02 17:06:07 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into alien values (101,'Erkan Özsoy','Computer Engineer');
-insert into alien values (102,'Emre Can Öztürk','Control Automotion Tech');
-insert into alien values (103,'Umut Can Kumcu','Lawyer');
-insert into alien values (104,'Ufuk Can Kumcu','Student');
-insert into alien values (105,'Deneme Dene','Okumaz bu çocuk');
-insert into alien values (107,'Deneme Özsoy','Computer Engineer');
+insert into alien values (101,'Elvis Presley','Singer');
+insert into alien values (102,'Michael Jackson','Computer Engineer');
+insert into alien values (103,'Justin Timberlake','Lawyer');
+insert into alien values (104,'Dimitri Vegas','Student');
+insert into alien values (105,'Ummet Ozcan','Singer');
+insert into alien values (107,'Martin Garrix','Computer Engineer');
